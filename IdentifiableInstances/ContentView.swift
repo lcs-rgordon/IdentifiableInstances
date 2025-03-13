1c7a87b9623cf337a1afb046d94ff48d619faf1e
Russell Gordon <rgordon@lcs.on.ca>
2025-03-13 09:13:55 -0400

    Initial Commit

---
+++ IdentifiableInstances/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IdentifiableInstances
+//
+//  Created by Russell Gordon on 2025-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IdentifiableInstances/IdentifiableInstancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentifiableInstancesApp.swift
+//  IdentifiableInstances
+//
+//  Created by Russell Gordon on 2025-03-13.
+//
+
+import SwiftUI
+
+@main
+struct IdentifiableInstancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
